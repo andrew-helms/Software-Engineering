7a626d4bc75b3b06533fd0a6b82e4d9fba9a4835
Andrew Helms <helmsandrew3@gmail.com>
2019-10-02 23:36:36 -0400

    Bootcamp 3

--- listings.server.model.js
+++ listings.server.model.js
@@ -1,11 +1,10 @@
+//You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
+
 /* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
-/* Create your schema for the data in the listings.json file that will define how data is saved in your database
-     See https://mongoosejs.com/docs/guide.html for examples for creating schemas
-     See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
-  */
+/* Create your schema */
 var listingSchema = new Schema({
 	code: {
 		type: String,
@@ -23,14 +22,11 @@ var listingSchema = new Schema({
 	created_at: Date,
 	updated_at: Date,
 	
-  //Check out - https://mongoosejs.com/docs/guide.html
 });
 
-/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
-   See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
-*/
+/* create a 'pre' function that adds the updated_at and created_at if not already there property */
 listingSchema.pre('save', function(next) {
-	var currentDate = new Date();
+  	var currentDate = new Date();
 	this.updated_at = currentDate;
 	
 	if (!this.created_at)
@@ -40,7 +36,6 @@ listingSchema.pre('save', function(next) {
 });
 
 /* Use your schema to instantiate a Mongoose model */
-//Check out - https://mongoosejs.com/docs/guide.html#models
 var Listing = mongoose.model('Listing', listingSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
